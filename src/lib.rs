0320176c30e32dedb12a78f8de97df135a7073f2
VrealZhik <you@example.com>
2025-11-27 15:32:33 +0300

    [f] return

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,27 @@
-#![cfg(mobile)]
-
 use tauri::{
-    plugin::{Builder, PluginHandle, TauriPlugin},
-    AppHandle, Manager, Runtime,
+    plugin::{Builder, TauriPlugin},
+    Manager, Runtime,
 };
 
 pub use models::*;
 
-// #[cfg(desktop)]
-// mod desktop;
-// #[cfg(mobile)]
-// mod mobile;
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
 
-// mod commands;
+mod commands;
 mod error;
 mod models;
 
-// use commands::*;
+use commands::*;
 
 pub use error::{Error, Result};
 
-#[cfg(target_os = "android")]
-const PLUGIN_IDENTIFIER: &str = "com.plugin.permissions";
-
-#[cfg(target_os = "ios")]
-tauri::ios_plugin_binding!(init_plugin_permissions);
-
-pub struct Permissions<R: Runtime>(PluginHandle<R>);
-
-impl<R: Runtime> Permissions<R> {
-    pub fn get_bluetooth_permission(
-        &self,
-        payload: PermissionRequest,
-    ) -> crate::Result<PermissionResult> {
-        self.0
-            .run_mobile_plugin("requestBluetoothPermissions", payload)
-            .map_err(Into::into)
-    }
-
-    pub fn get_location_permission(
-        &self,
-        payload: PermissionRequest,
-    ) -> crate::Result<PermissionResult> {
-        self.0
-            .run_mobile_plugin("requestLocationPermission", payload)
-            .map_err(Into::into)
-    }
-
-    pub fn get_notification_permission(
-        &self,
-        payload: PermissionRequest,
-    ) -> crate::Result<PermissionResult> {
-        self.0
-            .run_mobile_plugin("requestNotificationPermission", payload)
-            .map_err(Into::into)
-    }
-}
+#[cfg(desktop)]
+use desktop::Permissions;
+#[cfg(mobile)]
+use mobile::Permissions;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the permissions APIs.
 pub trait PermissionsExt<R: Runtime> {
@@ -71,12 +37,17 @@ impl<R: Runtime, T: Manager<R>> crate::PermissionsExt<R> for T {
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("permissions")
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![
+            get_bluetooth_permission,
+            get_location_permission,
+            get_notification_permission
+        ])
         .setup(|app, api| {
-            #[cfg(target_os = "android")]
-            let handle = api.register_android_plugin(PLUGIN_IDENTIFIER, "PermissionsPlugin")?;
-            #[cfg(target_os = "ios")]
-            let handle = api.register_ios_plugin(init_plugin_permissions)?;
+            #[cfg(mobile)]
+            let permissions = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let permissions = desktop::init(app, api)?;
+            app.manage(permissions);
             Ok(())
         })
         .build()
