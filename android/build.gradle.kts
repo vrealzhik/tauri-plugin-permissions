0d7e0ab8da428dc0dc7bb9a43552b65ff01f81fc
VrealZhik <you@example.com>
2025-11-27 16:04:31 +0300

    [f] gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
